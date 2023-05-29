a4df5e4f0729db9279726efa14b533f967035532
KSteinke <kacpersteinkesb@gmail.com>
2023-05-29 20:32:51 +0200

    Trobles_with_DB_migration
    To remember,  always set api project in PM console to perform migration or db update

---
+++ Chef_API/Chef_API/Migrations/20230529183054_update.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chef_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhotoName",
+                table: "Recipes",
+                newName: "RecipePhotoURL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RecipePhotoURL",
+                table: "Recipes",
+                newName: "PhotoName");
+        }
+    }
+}
