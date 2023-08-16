b7b6e20a2d2d5f04e1bcd4b9b2c6f3ea91b1dfc1
KSteinke <kacpersteinkesb@gmail.com>
2023-08-16 22:04:02 +0200

    LoginDto Validation attributes added

--- Chef_Models/Dtos/LoginDto.cs
+++ Chef_Models/Dtos/LoginDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace Chef_Models.Dtos
 {
     public class LoginDto
     {
-        public string UserName { get; set; }    
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Password { get; set; }
     }
 }
